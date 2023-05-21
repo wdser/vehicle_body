5f30e045971ee881d472101f54f4d811b0e161ab
wdser <mrwang_ds@163.com>
2023-05-21 20:33:09 +0800

    delete ros

--- vehicle_body.cpp
+++ vehicle_body.cpp
@@ -2,8 +2,6 @@
 #include <thread> // 包含std::thread头文件
 #include <cmath>
 
-// #include <ros/ros.h>
-// #include <sensor_msgs/LaserScan.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/io/obj_io.h>
@@ -88,6 +86,7 @@ bool IsLinePolygonMeshIntersected(
       polygon_viewer->removePointCloud("polygon_cloud");
       polygon_viewer->removePointCloud("intersect_point_on_mesh");
         polygon_viewer->addPointCloud(polygon_points_3d_ptr, "polygon_cloud");
+        polygon_viewer->addPointCloud(intersect_point_on_mesh, "intersect_point_on_mesh");
       for (size_t j =0 ; j < polygon.vertices.size(); j++) {
         auto index = polygon.vertices[j];
         pcl::PointXYZ polygon_point((*mesh_cloud)[index]);
@@ -103,11 +102,11 @@ bool IsLinePolygonMeshIntersected(
         polygon_viewer->setPointCloudRenderingProperties(
                 pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 10, "polygon_cloud");
 
-        polygon_viewer->addPolygon<pcl::PointXYZ>(polygon_points_3d_ptr, "polygon"+std::to_string(i));
-        polygon_viewer->setShapeRenderingProperties(
-                pcl::visualization::PCL_VISUALIZER_COLOR, 0, 1, 0, "polygon"+std::to_string(i));
-        polygon_viewer->setShapeRenderingProperties(
-                pcl::visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "polygon"+std::to_string(i));
+        // polygon_viewer->addPolygon<pcl::PointXYZ>(polygon_points_3d_ptr, "polygon"+std::to_string(i));
+        // polygon_viewer->setShapeRenderingProperties(
+        //         pcl::visualization::PCL_VISUALIZER_COLOR, 0, 1, 0, "polygon"+std::to_string(i));
+        // polygon_viewer->setShapeRenderingProperties(
+        //         pcl::visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "polygon"+std::to_string(i));
       polygon_viewer->spinOnce(10);
 
       // std::cout << "continue"<< std::endl;
@@ -136,20 +135,13 @@ bool IsLinePolygonMeshIntersected(
                 pcl::visualization::PCL_VISUALIZER_COLOR, 1.0, 0.0, 0, "intersect_point_on_mesh");
         polygon_viewer->setPointCloudRenderingProperties(
                 pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 15, "intersect_point_on_mesh");
-        polygon_viewer->spinOnce(500);
-        // while(!polygon_viewer->wasStopped())
-        // {
-        //   polygon_viewer->spinOnce(100);
-        //   std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        polygon_viewer->spinOnce(100);
+        // pcl::visualization::ShapeActorMapPtr shape_actor_map;
+        // shape_actor_map = polygon_viewer->getShapeActorMap();
+        // for (size_t remove_index = 0; remove_index <= i; remove_index++) {
+        //   std::string objectName = "polygon"+std::to_string(remove_index);  // 待删除的物体名称
+        //   polygon_viewer->removeShape(objectName);
         // }
-        polygon_viewer->spinOnce(500);
-        pcl::visualization::ShapeActorMapPtr shape_actor_map;
-        shape_actor_map = polygon_viewer->getShapeActorMap();
-        for (size_t remove_index = 0; remove_index <= i; remove_index++) {
-          std::string objectName = "polygon"+std::to_string(remove_index);  // 待删除的物体名称
-          polygon_viewer->removeShape(objectName);
-        }
-
         return is_in_polygon;
       }
     }
@@ -159,23 +151,6 @@ bool IsLinePolygonMeshIntersected(
 
 int main(int argc, char **argv)
 {
-  // ros::init(argc, argv, "my_ros_tool");
-  // ros::NodeHandle nh;
-
-  // 定义一个激光雷达数据
-  // ros::Publisher laser_pub = nh.advertise<sensor_msgs::LaserScan>("laser_scan", 1);
-  // sensor_msgs::LaserScan scan_msg;
-  // // 设置LaserScan消息的参数
-  // scan_msg.header.frame_id = "base_laser_link";
-  // scan_msg.angle_min = -M_PI / 2;
-  // scan_msg.angle_max = M_PI / 2;
-  // scan_msg.angle_increment = M_PI / 180;
-  // scan_msg.time_increment = 0.1;
-  // scan_msg.scan_time = 1;
-  // scan_msg.range_min = 0;
-  // scan_msg.range_max = 100;
-
-
   Eigen::Affine3f transform = Eigen::Affine3f::Identity();
   float angle = M_PI / 2;
   transform.rotate(Eigen::AngleAxisf(angle, Eigen::Vector3f::UnitX()));
@@ -221,7 +196,7 @@ int main(int argc, char **argv)
 
     for (size_t j = 0; j < total_yaw_counts; j++) {
     // for (size_t j = total_yaw_counts; j > 0 ; j--) {
-      if (j%200 != 0) continue;
+      if (j%100 != 0) continue;
       // viewer->removeShape("line"+std::to_string(i)+"_"+std::to_string(j));
       float pitch = (pitch_start_angle + i * pitch_resolution)/ 180 * 3.14;
       float yaw = (j * yaw_resolution) / 180 * 3.14;
@@ -267,20 +242,6 @@ int main(int argc, char **argv)
     // viewer->spin();
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
   }
-
-  // ros::Rate loop_rate(10);
-  // while (ros::ok())
-  // {
-  //   // 发布一个激光雷达数据
-  //   scan_msg.header.stamp = ros::Time::now();
-  //   for (int i = 0; i < scan_msg.ranges.size(); i++)
-  //   {
-  //     scan_msg.ranges[i] = i;
-  //   }
-  //   laser_pub.publish(scan_msg);
-
-  //   loop_rate.sleep();
-  // }
 
   return 0;
 }
